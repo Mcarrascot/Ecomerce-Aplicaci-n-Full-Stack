f2d485def57d1b06b495464a1f1f237539ed02be
Montserrat Carrasco <mfcarrascot@gmail.com>
2024-07-08 00:47:00 -0400

    feat: añadí login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
+import React from "react";
 import {
   RouterProvider,
 } from "react-router-dom";
-import './App.css'
-import NavBar from './components/Navbar';
-import Footer from './components/Footer';
-import { Routes } from './routes';
-function App() {
+import Navbar from "./components/Navbar/index";
 
+import Footer from "./components/Footer/Footer";
+import { Routes } from './routes';
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+function App() {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   return (
-    <>
-    <NavBar />
-    <RouterProvider router={Routes} />
-    <Footer />
-    </>
+    <div className="bg-white  duration-200">
+      <Navbar />
+      <RouterProvider router={Routes} />
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/pages/home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Hero from '../components/Hero/Hero';
+import TopProducts from '../components/TopProducts/TopProducts';
+import Subscribe from '../components/Subscribe/Subscribe';
+
+
+
+const Home = () => {
+  return (
+    <>
+        <Hero />
+        <TopProducts />
+        <Subscribe />
+    </>
+  );
+};
+
+export default Home;
