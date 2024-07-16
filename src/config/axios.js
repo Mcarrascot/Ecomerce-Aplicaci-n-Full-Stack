ad5bbe1003a22a8d78bf4cabd39e31b9928436f4
Montserrat Carrasco <mfcarrascot@gmail.com>
2024-07-16 16:52:09 -0400

    fix api url

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-const BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:1234/"
+// const BASE_URL = process.env.REACT_APP_BACKEND_URL || "https://ecommerce-api-production-5abe.up.railway.app/"
 const axiosClient = axios.create({
-    baseURL: BASE_URL,
+    baseURL: "https://ecommerce-api-production-5abe.up.railway.app/",
 })
 
 export default axiosClient
